525763581dbf90689d542ad237a040680620afb8
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-05-10 19:18:37 -0300

    Texte: A temperatura hoje está amena.

---
+++ estruturas_condicao1.py
@@ -0,0 +1,20 @@
+# Microatividade 1 - if e else
+
+#Crie uma variável chamada temperatura e atribua a ela o valor 29.
+
+#Crie uma verificação, utilizando a condição if, 
+#para checar se o valor da variável temperatura é menor que 30.
+
+#Caso positivo, imprima na tela o texto: "A temperatura hoje está amena";
+
+#Caso contrário, e utilizando a condição else, imprima na tela o texto: "Hoje está fazendo calor";
+
+#Ainda no script, altere o valor da variável temperatura para 31, 
+#salve a alteração e execute novamente o script.
+
+temperatura = 29
+
+if temperatura < 30:
+    print ("A temperatura hoje está amena")
+else:
+    print("Hoje está fazendo calor")
